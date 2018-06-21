254e4e56c640c86714c8a251e2e61287ce041125
Robert Long <robert@robertlong.me>
2018-06-21 11:44:12 -0700

    Fix eslint

--- src/client/components/gltf-type-mappings.js
+++ src/client/components/gltf-type-mappings.js
@@ -3,6 +3,7 @@ import { types } from "../editor/gltf-components";
 import NumericInput from "../components/NumericInput";
 import ColorInput from "../components/ColorInput";
 
+/* eslint react/display-name: 0 */
 export const gltfComponentTypeToReactComponent = new Map([
   [types.color, (value, onChange) => <ColorInput value={value} onChange={onChange} />],
   [types.number, (value, onChange) => <NumericInput value={value} onChange={onChange} />]
