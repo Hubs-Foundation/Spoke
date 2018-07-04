23b69108ef56d9c80e62f0d83b4f8e7742f2bf86
Robert Long <robert@robertlong.me>
2018-07-03 17:27:45 -0700

    eslint add padding-line-between-statements

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,19 @@ module.exports = {
     "no-throw-literal": "error",
     // Light console usage is useful but remove debug logs before merging to master.
     "no-console": "off",
-    "lines-between-class-members": 2
+    "lines-between-class-members": 2,
+    "padding-line-between-statements": [
+      "error",
+      { "blankLine": "always", "prev": "function", "next": "function" },
+      { "blankLine": "always", "prev": "function", "next": "class" },
+      { "blankLine": "always", "prev": "class", "next": "function" },
+      { "blankLine": "always", "prev": "class", "next": "export" },
+      { "blankLine": "always", "prev": "export", "next": "function" },
+      { "blankLine": "always", "prev": "export", "next": "class" },
+      { "blankLine": "always", "prev": "export", "next": "export" },
+      { "blankLine": "always", "prev": "import", "next": "function" },
+      { "blankLine": "always", "prev": "import", "next": "class" },
+    ]
   },
   extends: ["prettier", "plugin:react/recommended", "eslint:recommended"]
 };
