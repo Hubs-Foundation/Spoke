fc6a01d86f10df9ac26cf2fb2d7819b3cd8005e7
Brian Peiris <brianpeiris@gmail.com>
2018-07-10 16:39:59 -0700

    Add shadows back

--- src/client/editor/Editor.js
+++ src/client/editor/Editor.js
@@ -10,6 +10,7 @@ import SceneReferenceComponent from "./components/SceneReferenceComponent";
 import { loadScene, loadSerializedScene, serializeScene } from "./SceneLoader";
 import DirectionalLightComponent from "./components/DirectionalLightComponent";
 import AmbientLightComponent from "./components/AmbientLightComponent";
+import ShadowComponent from "./components/ShadowComponent";
 import { last } from "../utils";
 
 /**
@@ -271,6 +272,7 @@ export default class Editor {
     scene.traverse(child => {
       Object.defineProperty(child.userData, "_selectionRoot", { value: parent, enumerable: false });
     });
+    this.addComponent(scene, ShadowComponent.componentName);
 
     this.signals.objectAdded.dispatch(scene);
 
