8b119987f3abb95921a483a8514bebc828915ac5
tony056 <ytung@mozilla.com>
2018-07-10 17:23:48 -0700

    allowed color picker to show up after clicking on the color block.

--- src/client/ui/inputs/ColorInput.js
+++ src/client/ui/inputs/ColorInput.js
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { SketchPicker } from "react-color";
+import { EditableInput } from "react-color/lib/components/common";
+import styles from "./ColorInput.scss";
+import csscolorNames from "css-color-names";
 
-export default function ColorInput(props) {
-  return <SketchPicker color={props.value} onChange={color => props.onChange(color.hex)} />;
+export default class ColorInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayColorPicker: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState({displayColorPicker: !this.state.displayColorPicker});
+  };
+
+  handleClose = () => {
+    this.setState({displayColorPicker: false});
+  };
+
+  handleColorHexValue = () => {
+    const color = this.props.value;
+    return csscolorNames[color] ? csscolorNames[color] : color;
+  };
+
+  renderColorPicker = () => {
+    if (!this.state.displayColorPicker) {
+      return null;
+    }
+    return (
+      <div className={styles.popover}>
+        <div className={styles.cover} onClick={this.handleClose} />
+        <SketchPicker color={this.props.value} disableAlpha={true} onChange={(color) => this.props.onChange(color.hex)} />
+      </div>
+    );
+  };
+
+  render() {
+    return (
+      <div className={styles.colorInput}>
+        <div className={styles.block} onClick={this.handleClick}>
+          <div className={styles.color} style={{background: this.props.value}}/>
+        </div>
+        <div>
+          <EditableInput value={this.handleColorHexValue()} onChange={(color) => this.props.onChange(color)} />
+        </div>
+        {this.renderColorPicker()}
+      </div>
+    );
+  }
 }
 
 ColorInput.propTypes = {
