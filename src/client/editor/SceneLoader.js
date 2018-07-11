92855cd7763bf1d1b98785a7ee125e7cbd591a93
Robert Long <robert@robertlong.me>
2018-07-10 17:08:27 -0700

    First pass at export functionality

--- src/client/editor/SceneLoader.js
+++ src/client/editor/SceneLoader.js
@@ -1,6 +1,46 @@
 import THREE from "../vendor/three";
 import SceneReferenceComponent from "./components/SceneReferenceComponent";
 
+export function absoluteToRelativeURL(from, to) {
+  if (from === to) {
+    return to;
+  }
+
+  const fromURL = new URL(from, window.location);
+  const toURL = new URL(to, window.location);
+
+  if (fromURL.host === toURL.host) {
+    const relativeParts = [];
+    const fromParts = fromURL.pathname.split("/");
+    const toParts = toURL.pathname.split("/");
+
+    while (fromParts.length > 0 && toParts.length > 0 && fromParts[0] === toParts[0]) {
+      fromParts.shift();
+      toParts.shift();
+    }
+
+    if (fromParts.length > 1) {
+      for (let j = 0; j < fromParts.length - 1; j++) {
+        relativeParts.push("..");
+      }
+    }
+
+    for (let k = 0; k < toParts.length; k++) {
+      relativeParts.push(toParts[k]);
+    }
+
+    const relativePath = relativeParts.join("/");
+
+    if (relativePath.startsWith("../")) {
+      return relativePath;
+    }
+
+    return "./" + relativePath;
+  }
+
+  return to;
+}
+
 function loadGLTF(url) {
   return new Promise((resolve, reject) => {
     new THREE.GLTFLoader().load(
@@ -19,6 +59,35 @@ function loadGLTF(url) {
       }
     );
   });
+}
+
+function postProcessGLTF(scene, sceneURI, gltf) {
+  const { json } = gltf;
+
+  const buffers = json.buffers;
+
+  if (buffers && buffers.length > 0 && buffers[0].uri === undefined) {
+    buffers[0].uri = scene.name + ".bin";
+  }
+
+  const absoluteSceneURI = new URL(sceneURI, window.location).href;
+
+  if (Array.isArray(json.images)) {
+    for (const image of json.images) {
+      image.uri = absoluteToRelativeURL(absoluteSceneURI, image.uri);
+    }
+  }
+
+  return gltf;
+}
+
+export function exportScene(scene, sceneURI) {
+  return new Promise(resolve => {
+    new THREE.GLTFExporter().parseParts(scene, resolve, {
+      embedImages: false,
+      onlyVisible: false
+    });
+  }).then(gltf => postProcessGLTF(scene, sceneURI, gltf));
 }
 
 function inflateGLTFComponents(scene, addComponent) {
@@ -118,40 +187,6 @@ function resolveRelativeURLs(entities, absoluteSceneURL) {
       }
     }
   }
-}
-
-export function absoluteToRelativeURL(from, to) {
-  if (from === to) {
-    return to;
-  }
-
-  const fromURL = new URL(from, window.location);
-  const toURL = new URL(to, window.location);
-
-  if (fromURL.host === toURL.host) {
-    const relativeParts = [];
-    const fromParts = fromURL.pathname.split("/");
-    const toParts = toURL.pathname.split("/");
-
-    while (fromParts.length > 0 && toParts.length > 0 && fromParts[0] === toParts[0]) {
-      fromParts.shift();
-      toParts.shift();
-    }
-
-    if (fromParts.length > 1) {
-      for (let j = 0; j++; j < fromParts.length - 1) {
-        relativeParts.push("..");
-      }
-    }
-
-    for (let k = 0; k < toParts.length; k++) {
-      relativeParts.push(toParts[k]);
-    }
-
-    return "./" + relativeParts.join("/");
-  }
-
-  return to;
 }
 
 function convertAbsoluteURLs(entities, sceneURL) {
