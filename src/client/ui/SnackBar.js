bfd9858ed95109e8d41e6f9d112fd09666067a1a
tony056 <ytung@mozilla.com>
2018-07-12 16:30:10 -0700

    displayed contents by conflict types.

--- src/client/ui/SnackBar.js
+++ src/client/ui/SnackBar.js
@@ -1,14 +1,45 @@
 import React from "react";
 import styles from "./SnackBar.scss";
+import errorIcon from "../assets/error-icon.svg";
+import warningIcon from "../assets/warning-icon.svg";
+import PropTypes from "prop-types";
+import Button from "./Button";
+import classNames from "classnames";
 
-export default function SnackBar() {
+export default function SnackBar({ conflictType }) {
+  const conflictTypes = {
+    missing: {
+      img: errorIcon,
+      content: "Missing nodes in your gltf modals.",
+      helpURL: "https://github.com/MozillaReality/hubs-editor/wiki/Tutorials"
+    },
+    duplicate: {
+      img: warningIcon,
+      content: "Duplicate node names in your gltf modals.",
+      helpURL: "https://github.com/MozillaReality/hubs-editor/wiki/Tutorials"
+    }
+  };
+  console.log('type: ' + conflictType);
   return (
     <div className={styles.bar}>
       <div className={styles.icon}>
-        <image src="" />
+        <img src={conflictTypes[conflictType].img} />
       </div>
-      <div className={styles.content}>Content</div>
-      <div className={styles.options}>Options</div>
+      <div
+        className={classNames(styles.content, {
+          error: conflictType === "missing",
+          warning: conflictType === "duplicate"
+        })}
+      >
+        {conflictTypes[conflictType].content}
+      </div>
+      <div className={styles.options}>
+        <Button onClick={() => window.open(conflictTypes[conflictType].helpURL)}>Help</Button>
+      </div>
     </div>
   );
 }
+
+SnackBar.propTypes = {
+  conflictType: PropTypes.string
+};
