cc6582d0cadc79086a763b15266c46831e3f0a26
Tony Tung <ytung@mozilla.com>
2018-08-20 16:57:33 -0700

    Made color picker popped out on the top and follow the color block position.

--- src/client/ui/inputs/ColorInput.js
+++ src/client/ui/inputs/ColorInput.js
@@ -9,12 +9,37 @@ export default class ColorInput extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      targetBlock: null,
+      pickerX: 0,
+      pickerY: 0,
       displayColorPicker: false
     };
   }
 
-  handleClick = () => {
-    this.setState({ displayColorPicker: !this.state.displayColorPicker });
+  componentDidMount = () => {
+    window.addEventListener("resize", this.updateDimensions);
+  };
+
+  componentWillUnmount = () => {
+    window.removeEventListener("resize", this.updateDimensions);
+  };
+
+  updateDimensions = () => {
+    const target = this.state.targetBlock.getBoundingClientRect();
+    this.setState({
+      pickerX: target.left,
+      pickerY: target.y + target.height
+    });
+  };
+
+  handleClick = e => {
+    const target = e.currentTarget.getBoundingClientRect();
+    this.setState({
+      targetBlock: e.currentTarget,
+      pickerX: target.left,
+      pickerY: target.y + target.height,
+      displayColorPicker: !this.state.displayColorPicker
+    });
   };
 
   handleClose = () => {
@@ -30,8 +55,12 @@ export default class ColorInput extends Component {
     if (!this.state.displayColorPicker) {
       return null;
     }
+    const pos = {
+      left: this.state.pickerX,
+      top: this.state.pickerY
+    };
     return (
-      <div className={styles.popover}>
+      <div style={pos} className={styles.popover}>
         <div className={styles.cover} onClick={this.handleClose} />
         <div className={styles.colorPicker}>
           <SketchPicker
