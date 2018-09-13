40480e2a6fc46b38db18662d8167658abbf34400
Greg Fodor <gfodor@gmail.com>
2018-09-13 16:32:06 +0000

    UX cleanup for removal of Add Node menu

--- src/client/ui/panels/HierarchyPanelContainer.js
+++ src/client/ui/panels/HierarchyPanelContainer.js
@@ -195,9 +195,10 @@ class HierarchyPanelContainer extends Component {
   renderHierarchyNodeMenu = props => {
     const node = props.trigger;
     const hasParent = node && node.object.parent;
+    if (!hasParent) return null;
+
     return (
       <ContextMenu id="hierarchy-node-menu">
-        {hasParent && <MenuItem divider />}
         {hasParent && (
           <MenuItem onClick={this.onDuplicateNode}>
             Duplicate
