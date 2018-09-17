3b4f7607a60115c5aa45d09685d1941a7ce5509d
Greg Fodor <gfodor@gmail.com>
2018-09-17 18:15:26 +0000

    Improved error messaging for Sketchfab and Poly import

--- src/client/utils/import.js
+++ src/client/utils/import.js
@@ -11,9 +11,21 @@ export async function performModelImport(url, editor, showDialog, hideDialog) {
     await editor.importGLTFIntoModelNode(url);
     hideDialog();
   } catch (e) {
+    let message = e.message;
+
+    if (url.indexOf("sketchfab.com") >= 0) {
+      message =
+        "Error adding model.\n\nNote: Sketchfab models must be marked as 'Downloadable' to be added to your scene.\n\nError: " +
+        e.message;
+    } else if (url.indexOf("poly.google.com") >= 0) {
+      message =
+        "Error adding model.\n\nNote: Poly panoramas are not supported and 3D models must be GLTF 2.0.\n\nError: " +
+        e.message;
+    }
+
     showDialog(ErrorDialog, {
-      title: "Error adding model.",
-      message: e.message
+      title: "Add Model",
+      message: message
     });
   }
 }
