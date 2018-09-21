c7441c04ff093213be3dca12d53ba5d737e06868
Greg Fodor <gfodor@gmail.com>
2018-09-21 18:58:23 +0000

    Toggle visible component on/off based on selection

--- src/client/editor/components/NavMeshComponent.js
+++ src/client/editor/components/NavMeshComponent.js
@@ -3,6 +3,8 @@ import BaseComponent from "./BaseComponent";
 export default class NavMeshComponent extends BaseComponent {
   static componentName = "nav-mesh";
 
+  static componentDescription = "Sets the walkable surface area in your scene.";
+
   static iconClassName = "fa-shoe-prints";
 
   static schema = [];
