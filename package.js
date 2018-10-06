7b764edacca7670a50d05b99a32ee0ebde19868b
Brian Peiris <brianpeiris@gmail.com>
2018-10-06 07:46:14 -0700

    Restore zip file releases

--- package.js
+++ package.js
@@ -1,15 +1,44 @@
-// Builds an executable for each platform that will run the server hosting the editor code.
+// Builds a release archive for each platform, containing an executable for that platform
+// that will run the server hosting the editor code.
 // Outputs the executables and archives into the /release directory.
 
 const { exec } = require("pkg");
+const util = require("util");
+const fs = require("fs");
+const path = require("path");
+const JSZip = require("jszip");
 
 function buildRelease(targets, outPath, opts) {
   const args = ["bin/spoke", "--config", ".pkg.json", "--target", targets.join(","), "--out-path", outPath];
   return exec(args.concat(opts));
 }
 
+function appendExtension(executableName, platform) {
+  return platform === "win" ? `${executableName}.exe` : executableName;
+}
+
 const outputDir = "release";
 const platforms = ["linux", "macos", "win"];
 const targets = platforms.map(p => `node10-${p}-x64`);
+const readFilePromise = util.promisify(fs.readFile);
 
-buildRelease(targets, outputDir, process.argv.slice(2));
+buildRelease(targets, outputDir, process.argv.slice(2)).then(() => {
+  for (const platform of platforms) {
+    const executableName = `spoke-${platform}`;
+    const executablePath = path.join(outputDir, appendExtension(executableName, platform));
+
+    const zip = new JSZip();
+    const spoke = zip.folder("Spoke");
+    spoke.file(appendExtension("spoke", platform), readFilePromise(executablePath), { unixPermissions: 0o775 });
+
+    const archivePath = path.join(outputDir, `${executableName}.zip`);
+    const archiveStream = zip.generateNodeStream({
+      type: "nodebuffer",
+      streamFiles: true,
+      compression: "DEFLATE",
+      compressionOptions: { level: 5 },
+      platform: platform === "win" ? "DOS" : "UNIX"
+    });
+    archiveStream.pipe(fs.createWriteStream(archivePath));
+  }
+});
