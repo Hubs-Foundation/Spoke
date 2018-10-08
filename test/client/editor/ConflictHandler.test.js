29e4ec3fa7c9813f5c63df975c438125fb06cbd2
Brian Peiris <brianpeiris@gmail.com>
2018-10-07 18:11:02 -0700

    Fix tests to account for previous conflict handler changes

--- test/client/editor/ConflictHandler.test.js
+++ test/client/editor/ConflictHandler.test.js
@@ -4,8 +4,8 @@ import test from "ava";
 test("Reused names are given an index", t => {
   const handler = new ConflictHandler();
   t.is("new", handler.addToDuplicateNameCounters("new"));
-  t.is("new_1", handler.addToDuplicateNameCounters("new"));
-  t.is("new_2", handler.addToDuplicateNameCounters("new"));
+  t.is("new 2", handler.addToDuplicateNameCounters("new"));
+  t.is("new 3", handler.addToDuplicateNameCounters("new"));
   t.is("foo", handler.addToDuplicateNameCounters("foo"));
 });
 
@@ -18,41 +18,41 @@ test("Name can be reused if removed", t => {
 
 test("Indexed name can be reused if removed", t => {
   const handler = new ConflictHandler();
-  t.is("foo_1", handler.addToDuplicateNameCounters("foo_1"));
-  handler.removeFromDuplicateNameCounters("foo_1");
-  t.is("foo_1", handler.addToDuplicateNameCounters("foo_1"));
+  t.is("foo 2", handler.addToDuplicateNameCounters("foo 2"));
+  handler.removeFromDuplicateNameCounters("foo 2");
+  t.is("foo 2", handler.addToDuplicateNameCounters("foo 2"));
 });
 
 test("Indexing works when previous name is removed and readded", t => {
   const handler = new ConflictHandler();
   t.is("new", handler.addToDuplicateNameCounters("new"));
 
-  t.is("new_1", handler.addToDuplicateNameCounters("new"));
+  t.is("new 2", handler.addToDuplicateNameCounters("new"));
   handler.removeFromDuplicateNameCounters("new");
 
   t.is("new", handler.addToDuplicateNameCounters("new"));
-  t.is("new_2", handler.addToDuplicateNameCounters("new"));
+  t.is("new 3", handler.addToDuplicateNameCounters("new"));
 });
 
 test("Name can be used if index is used first", t => {
   const handler = new ConflictHandler();
-  t.is("new_1", handler.addToDuplicateNameCounters("new_1"));
+  t.is("new 2", handler.addToDuplicateNameCounters("new 2"));
   t.is("new", handler.addToDuplicateNameCounters("new"));
-  t.is("new_2", handler.addToDuplicateNameCounters("new"));
+  t.is("new 3", handler.addToDuplicateNameCounters("new"));
 });
 
 test("Reuse should be disallowed even after indexed name is removed", t => {
   const handler = new ConflictHandler();
   t.is("foo", handler.addToDuplicateNameCounters("foo"));
-  t.is("foo_1", handler.addToDuplicateNameCounters("foo_1"));
-  handler.removeFromDuplicateNameCounters("foo_1");
+  t.is("foo 2", handler.addToDuplicateNameCounters("foo 2"));
+  handler.removeFromDuplicateNameCounters("foo 2");
   t.false(handler.isUniqueObjectName("foo"));
 });
 
 test("Indexing increments even after intermediate index is removed", t => {
   const handler = new ConflictHandler();
   t.is("foo", handler.addToDuplicateNameCounters("foo"));
-  t.is("foo_1", handler.addToDuplicateNameCounters("foo"));
-  handler.removeFromDuplicateNameCounters("foo_1");
-  t.is("foo_2", handler.addToDuplicateNameCounters("foo"));
+  t.is("foo 2", handler.addToDuplicateNameCounters("foo"));
+  handler.removeFromDuplicateNameCounters("foo 2");
+  t.is("foo 3", handler.addToDuplicateNameCounters("foo"));
 });
