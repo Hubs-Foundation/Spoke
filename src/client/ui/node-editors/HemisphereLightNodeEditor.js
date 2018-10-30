c552de00e4c5d2c82b1d5ca97aa3f1046d13fe95
Robert Long <robert@robertlong.me>
2018-10-30 15:54:40 -0700

    Light node editors

--- src/client/ui/node-editors/HemisphereLightNodeEditor.js
+++ src/client/ui/node-editors/HemisphereLightNodeEditor.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import NodeEditor from "./NodeEditor";
+import InputGroup from "../InputGroup";
+import ColorInput from "../inputs/ColorInput";
+import BooleanInput from "../inputs/BooleanInput";
+import CompoundNumericInput from "../inputs/CompoundNumericInput";
 
 export default class HemisphereLightNodeEditor extends Component {
   static propTypes = {
@@ -10,7 +14,40 @@ export default class HemisphereLightNodeEditor extends Component {
 
   static iconClassName = "fa-certificate";
 
+  onChangeSkyColor = skyColor => {
+    this.props.editor.setNodeProperty(this.props.node, "skyColor", skyColor);
+  };
+
+  onChangeGroundColor = groundColor => {
+    this.props.editor.setNodeProperty(this.props.node, "groundColor", groundColor);
+  };
+
+  onChangeIntensity = intensity => {
+    this.props.editor.setNodeProperty(this.props.node, "intensity", intensity);
+  };
+
+  onChangeCastShadow = castShadow => {
+    this.props.editor.setNodeProperty(this.props.node, "castShadow", castShadow);
+  };
+
   render() {
-    return <NodeEditor {...this.props} description="A light which illuminates the scene from directly overhead." />;
+    const node = this.props.node;
+
+    return (
+      <NodeEditor {...this.props} description="A light which illuminates the scene from directly overhead.">
+        <InputGroup name="Sky Color">
+          <ColorInput value={node.skyColor} onChange={this.onChangeSkyColor} />
+        </InputGroup>
+        <InputGroup name="Ground Color">
+          <ColorInput value={node.groundColor} onChange={this.onChangeGroundColor} />
+        </InputGroup>
+        <InputGroup name="Intensity">
+          <CompoundNumericInput value={node.intensity} onChange={this.onChangeIntensity} />
+        </InputGroup>
+        <InputGroup name="Cast Shadow">
+          <BooleanInput value={node.castShadow} onChange={this.onChangeCastShadow} />
+        </InputGroup>
+      </NodeEditor>
+    );
   }
 }
--- src/client/ui/node-editors/PointLightNodeEditor.js
+++ src/client/ui/node-editors/PointLightNodeEditor.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import NodeEditor from "./NodeEditor";
+import InputGroup from "../InputGroup";
+import ColorInput from "../inputs/ColorInput";
+import BooleanInput from "../inputs/BooleanInput";
+import CompoundNumericInput from "../inputs/CompoundNumericInput";
 
 export default class PointLightNodeEditor extends Component {
   static propTypes = {
@@ -10,7 +14,33 @@ export default class PointLightNodeEditor extends Component {
 
   static iconClassName = "fa-lightbulb";
 
+  onChangeColor = color => {
+    this.props.editor.setNodeProperty(this.props.node, "color", color);
+  };
+
+  onChangeIntensity = intensity => {
+    this.props.editor.setNodeProperty(this.props.node, "intensity", intensity);
+  };
+
+  onChangeCastShadow = castShadow => {
+    this.props.editor.setNodeProperty(this.props.node, "castShadow", castShadow);
+  };
+
   render() {
-    return <NodeEditor {...this.props} description="A light which emits in all directions from a single point." />;
+    const node = this.props.node;
+
+    return (
+      <NodeEditor {...this.props} description="A light which emits in all directions from a single point.">
+        <InputGroup name="Color">
+          <ColorInput value={node.color} onChange={this.onChangeColor} />
+        </InputGroup>
+        <InputGroup name="Intensity">
+          <CompoundNumericInput value={node.intensity} onChange={this.onChangeIntensity} />
+        </InputGroup>
+        <InputGroup name="Cast Shadow">
+          <BooleanInput value={node.castShadow} onChange={this.onChangeCastShadow} />
+        </InputGroup>
+      </NodeEditor>
+    );
   }
 }
