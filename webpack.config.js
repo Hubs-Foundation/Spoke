8b6b6256b6d01cf8fb03f7d8dcf59413334e381e
Robert Long <robert@robertlong.me>
2019-04-04 11:47:49 -0700

    Fix source maps

--- webpack.config.js
+++ webpack.config.js
@@ -70,166 +70,177 @@ function createHTTPSConfig() {
 const defaultHostName = "hubs.local";
 const host = process.env.HOST_IP || defaultHostName;
 
-module.exports = env => ({
-  mode: process.env.NODE_ENV ? "development" : "production",
-
-  entry: {
-    entry: ["./src/index.js"]
-  },
-
-  devtool: process.env.NODE_ENV === "production" ? "source-map" : "inline-source-map",
-
-  devServer: {
-    https: createHTTPSConfig(),
-    historyApiFallback: true,
-    port: 9090,
-    host: process.env.HOST_IP || "0.0.0.0",
-    public: `${host}:9090`,
-    publicPath: process.env.BASE_ASSETS_PATH || "",
-    useLocalIp: true,
-    allowedHosts: [host],
-    headers: {
-      "Access-Control-Allow-Origin": "*"
+module.exports = env => {
+  return {
+    entry: {
+      entry: ["./src/index.js"]
     },
-    before: function(app) {
-      // be flexible with people accessing via a local reticulum on another port
-      app.use(cors({ origin: /hubs\.local(:\d*)?$/ }));
-    }
-  },
 
-  output: {
-    filename: "assets/js/[name]-[chunkhash].js",
-    publicPath: process.env.BASE_ASSETS_PATH || "/"
-  },
+    devtool: process.env.NODE_ENV === "production" ? "source-map" : "inline-source-map",
 
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpg|jpeg|gif|svg)(\?.*$|$)/,
-        use: {
-          loader: "file-loader",
-          options: {
-            name: "[name]-[hash].[ext]",
-            outputPath: "assets/images"
-          }
-        }
+    devServer: {
+      https: createHTTPSConfig(),
+      historyApiFallback: true,
+      port: 9090,
+      host: process.env.HOST_IP || "0.0.0.0",
+      public: `${host}:9090`,
+      publicPath: process.env.BASE_ASSETS_PATH || "",
+      useLocalIp: true,
+      allowedHosts: [host],
+      headers: {
+        "Access-Control-Allow-Origin": "*"
       },
-      {
-        test: /\.(woff|woff2|ttf|eot)(\?.*$|$)/,
-        use: {
-          loader: "file-loader",
-          options: {
-            name: "[name]-[hash].[ext]",
-            outputPath: "assets/fonts"
-          }
-        }
-      },
-      {
-        test: /\.(glb)(\?.*$|$)/,
-        use: {
-          loader: "file-loader",
-          options: {
-            name: "[name]-[hash].[ext]",
-            outputPath: "assets/models"
-          }
-        }
-      },
-      {
-        test: /\.(mp4|webm)(\?.*$|$)/,
-        use: {
-          loader: "file-loader",
-          options: {
-            name: "[name]-[hash].[ext]",
-            outputPath: "assets/videos"
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [process.env.NODE_ENV !== "production" ? "style-loader" : MiniCssExtractPlugin.loader, "css-loader"]
-      },
-      {
-        test: /\.scss$/,
-        include: path.join(__dirname, "src"),
-        use: [
-          process.env.NODE_ENV !== "production" ? "style-loader" : MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader"
-        ]
-      },
-      {
-        test: /\.js$/,
-        include: path.join(__dirname, "src"),
-        use: "babel-loader"
-      },
-      {
-        test: /\.worker\.js$/,
-        include: path.join(__dirname, "src"),
-        loader: "worker-loader",
-        options: {
-          name: "assets/js/workers/[name]-[hash].js",
-          publicPath: "/",
-          inline: true
-        }
-      },
-      {
-        test: /\.wasm$/,
-        type: "javascript/auto",
-        include: path.join(__dirname, "src"),
-        use: {
-          loader: "file-loader",
-          options: {
-            outputPath: "assets/js/wasm",
-            name: "[name]-[hash].[ext]"
-          }
-        }
+      before: function(app) {
+        // be flexible with people accessing via a local reticulum on another port
+        app.use(cors({ origin: /hubs\.local(:\d*)?$/ }));
       }
+    },
+
+    output: {
+      filename: "assets/js/[name]-[chunkhash].js",
+      publicPath: process.env.BASE_ASSETS_PATH || "/"
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(png|jpg|jpeg|gif|svg)(\?.*$|$)/,
+          use: {
+            loader: "file-loader",
+            options: {
+              name: "[name]-[hash].[ext]",
+              outputPath: "assets/images"
+            }
+          }
+        },
+        {
+          test: /\.(woff|woff2|ttf|eot)(\?.*$|$)/,
+          use: {
+            loader: "file-loader",
+            options: {
+              name: "[name]-[hash].[ext]",
+              outputPath: "assets/fonts"
+            }
+          }
+        },
+        {
+          test: /\.(glb)(\?.*$|$)/,
+          use: {
+            loader: "file-loader",
+            options: {
+              name: "[name]-[hash].[ext]",
+              outputPath: "assets/models"
+            }
+          }
+        },
+        {
+          test: /\.(mp4|webm)(\?.*$|$)/,
+          use: {
+            loader: "file-loader",
+            options: {
+              name: "[name]-[hash].[ext]",
+              outputPath: "assets/videos"
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [process.env.NODE_ENV !== "production" ? "style-loader" : MiniCssExtractPlugin.loader, "css-loader"]
+        },
+        {
+          test: /\.scss$/,
+          include: path.join(__dirname, "src"),
+          use: [
+            process.env.NODE_ENV !== "production" ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader"
+          ]
+        },
+        {
+          test: /\.js$/,
+          include: path.join(__dirname, "src"),
+          use: "babel-loader"
+        },
+        {
+          test: /\.worker\.js$/,
+          include: path.join(__dirname, "src"),
+          loader: "worker-loader",
+          options: {
+            name: "assets/js/workers/[name]-[hash].js",
+            publicPath: "/",
+            inline: true
+          }
+        },
+        {
+          test: /\.wasm$/,
+          type: "javascript/auto",
+          include: path.join(__dirname, "src"),
+          use: {
+            loader: "file-loader",
+            options: {
+              outputPath: "assets/js/wasm",
+              name: "[name]-[hash].[ext]"
+            }
+          }
+        }
+      ]
+    },
+
+    resolve: {
+      alias: {
+        three$: path.join(__dirname, "node_modules/three/build/three.module.js")
+      }
+    },
+
+    optimization: {
+      minimizer: [
+        new TerserJSPlugin({ sourceMap: true }),
+        new OptimizeCSSAssetsPlugin({
+          cssProcessorOptions: {
+            sourcemap: true,
+            map: {
+              inline: false,
+              annotation: true
+            }
+          }
+        })
+      ]
+    },
+
+    plugins: [
+      new BundleAnalyzerPlugin({
+        analyzerMode: env && env.BUNDLE_ANALYZER ? "server" : "disabled"
+      }),
+      new CopyWebpackPlugin([
+        {
+          from: path.join(__dirname, "src", "assets", "favicon-spoke.ico"),
+          to: "assets/images/favicon-spoke.ico"
+        }
+      ]),
+      new HTMLWebpackPlugin({
+        template: path.join(__dirname, "src", "index.html"),
+        faviconPath: (process.env.BASE_ASSETS_PATH || "/") + "assets/images/favicon-spoke.ico"
+      }),
+      new webpack.DefinePlugin({
+        SPOKE_VERSION: JSON.stringify(packageJSON.version)
+      }),
+      new webpack.EnvironmentPlugin({
+        NODE_ENV: "development",
+        RETICULUM_SERVER: undefined,
+        FARSPARK_SERVER: undefined,
+        HUBS_SERVER: undefined,
+        CORS_PROXY_SERVER: null,
+        BASE_ASSETS_PATH: "",
+        NON_CORS_PROXY_DOMAINS: "",
+        ROUTER_BASE_PATH: ""
+      }),
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^@blueprintjs\//
+      }),
+      new MiniCssExtractPlugin({
+        filename: "assets/styles/[name]-[contenthash].css",
+        chunkFilename: "assets/styles/[name]-[contenthash].css"
+      })
     ]
-  },
-
-  resolve: {
-    alias: {
-      three$: path.join(__dirname, "node_modules/three/build/three.module.js")
-    }
-  },
-
-  optimization: {
-    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
-  },
-
-  plugins: [
-    new BundleAnalyzerPlugin({
-      analyzerMode: env && env.BUNDLE_ANALYZER ? "server" : "disabled"
-    }),
-    new CopyWebpackPlugin([
-      {
-        from: path.join(__dirname, "src", "assets", "favicon-spoke.ico"),
-        to: "assets/images/favicon-spoke.ico"
-      }
-    ]),
-    new HTMLWebpackPlugin({
-      template: path.join(__dirname, "src", "index.html"),
-      faviconPath: (process.env.BASE_ASSETS_PATH || "/") + "assets/images/favicon-spoke.ico"
-    }),
-    new webpack.DefinePlugin({
-      SPOKE_VERSION: JSON.stringify(packageJSON.version)
-    }),
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: "development",
-      RETICULUM_SERVER: undefined,
-      FARSPARK_SERVER: undefined,
-      HUBS_SERVER: undefined,
-      CORS_PROXY_SERVER: null,
-      BASE_ASSETS_PATH: "",
-      NON_CORS_PROXY_DOMAINS: "",
-      ROUTER_BASE_PATH: ""
-    }),
-    new webpack.IgnorePlugin({
-      resourceRegExp: /^@blueprintjs\//
-    }),
-    new MiniCssExtractPlugin({
-      filename: "assets/styles/[name]-[contenthash].css",
-      chunkFilename: "assets/styles/[name]-[contenthash].css"
-    })
-  ]
-});
+  };
+};
