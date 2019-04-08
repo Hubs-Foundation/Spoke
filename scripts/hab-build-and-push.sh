58fe23696f37189e215f43630dd256e827f58dd3
Robert Long <robert@robertlong.me>
2019-04-08 16:01:41 -0700

    Fix npm caching

--- scripts/hab-build-and-push.sh
+++ scripts/hab-build-and-push.sh
@@ -24,6 +24,7 @@ rm /usr/bin/env
 ln -s "$(hab pkg path core/coreutils)/bin/env" /usr/bin/env
 hab pkg install -b core/coreutils core/bash core/node10 core/git core/aws-cli core/python2
 
+npm config set cache "$(pwd)/.npm" # Set the npm cache to a directory in the current workspace so that it can be reused across ci builds
 npm ci --verbose --no-progress
 npm rebuild node-sass # HACK sometimes node-sass build fails
 npm rebuild node-sass # HACK sometimes node-sass build fails
