fbaa9bfc6443ce1cba3ee60b2faa8d86129ec729
johnshaughnessy <johnfshaughnessy@gmail.com>
2019-05-10 20:05:09 -0700

    Add mouse look to fly controls

--- src/editor/controls/FlyControls.js
+++ src/editor/controls/FlyControls.js
@@ -1,6 +1,17 @@
 import { Fly, FlyMapping } from "./input-mappings";
 import THREE from "../../vendor/three";
 
+const EPSILON = 10e-5;
+const UP = new THREE.Vector3(0, 1, 0);
+const parentInverse = new THREE.Matrix4();
+const IDENTITY = new THREE.Matrix4().identity();
+const vec = new THREE.Vector3();
+const quat = new THREE.Quaternion();
+const worldPos = new THREE.Vector3();
+const worldQuat = new THREE.Quaternion();
+const worldScale = new THREE.Vector3();
+const candidateWorldQuat = new THREE.Quaternion();
+
 export default class FlyControls {
   constructor(camera, inputManager) {
     this.enabled = false;
@@ -8,50 +19,86 @@ export default class FlyControls {
     this.inputManager = inputManager;
     this.moveSpeed = 2;
     this.boostSpeed = 2;
-    this.lookSensitivity = 1;
+    this.lookSensitivity = 10;
+    this.maxXRotation = THREE.Math.degToRad(80);
     this.direction = new THREE.Vector3();
-    this.maxXRotation = THREE.Math.degToRad(70);
-  }
 
-  onClickCanvas = () => {
-    if (this.enabled && document.pointerLockElement !== this.inputManager.canvas) {
-      this.inputManager.canvas.requestPointerLock();
-    }
-  };
+    this.onMouseUp = e => {
+      if (this.enabled && e.button === 2) {
+        if (document.pointerLockElement === this.inputManager.canvas) {
+          document.exitPointerLock();
+        }
+      }
+    };
+  }
 
   enable() {
     this.enabled = true;
+    this.inputManager.canvas.requestPointerLock();
     this.inputManager.enableInputMapping(Fly, FlyMapping);
+    this.inputManager.canvas.addEventListener("mouseup", this.onMouseUp);
   }
 
   disable() {
     this.enabled = false;
     this.inputManager.disableInputMapping(Fly);
+    this.inputManager.canvas.removeEventListener("mouseup", this.onMouseUp);
   }
 
   update(dt) {
     if (!this.enabled) return;
-
     const input = this.inputManager;
 
-    const rotation = this.camera.rotation;
-    const lookSensitivity = this.lookSensitivity;
+    // assume that this.camera[position,quaterion/rotation,scale] are authority
+    this.camera.updateMatrix();
+    this.camera.updateMatrixWorld();
+    this.camera.matrixWorld.decompose(worldPos, worldQuat, worldScale);
 
-    let xRotation = rotation.x > Math.PI ? rotation.x - 2 * Math.PI : rotation.x;
-    xRotation = THREE.Math.clamp(xRotation, -this.maxXRotation, this.maxXRotation);
-
-    rotation.set(
-      xRotation + input.get(Fly.lookY) * lookSensitivity,
-      rotation.y + input.get(Fly.lookX) * lookSensitivity,
-      0,
-      "ZYX"
+    // rotate about the camera's local x axis
+    candidateWorldQuat.multiplyQuaternions(
+      quat.setFromAxisAngle(vec.set(1, 0, 0).applyQuaternion(worldQuat), input.get(Fly.lookY) * this.lookSensitivity),
+      worldQuat
     );
 
-    this.direction.set(input.get(Fly.moveX), 0, input.get(Fly.moveZ));
+    // check change of local "forward" and "up" to disallow flipping
+    const camUpY = vec.set(0, 1, 0).applyQuaternion(worldQuat).y;
+    const newCamUpY = vec.set(0, 1, 0).applyQuaternion(candidateWorldQuat).y;
+    const newCamForwardY = vec.set(0, 0, -1).applyQuaternion(candidateWorldQuat).y;
+    const extrema = Math.sin(this.maxXRotation);
+    const allowRotationInX =
+      newCamUpY > 0 && ((newCamForwardY < extrema && newCamForwardY > -extrema) || newCamUpY > camUpY);
+    if (allowRotationInX) {
+      this.camera.matrixWorld.compose(
+        worldPos,
+        candidateWorldQuat,
+        worldScale
+      );
+      // assume that if camera.parent exists, its matrixWorld is up to date
+      parentInverse.getInverse(this.camera.parent ? this.camera.parent.matrixWorld : IDENTITY);
+      this.camera.matrix.multiplyMatrices(parentInverse, this.camera.matrixWorld);
+      this.camera.matrixWorld.decompose(this.camera.position, this.camera.quaternion, this.camera.scale);
+    }
 
+    this.camera.matrixWorld.decompose(worldPos, worldQuat, worldScale);
+    // rotate about the world y axis
+    candidateWorldQuat.multiplyQuaternions(
+      quat.setFromAxisAngle(UP, input.get(Fly.lookX) * this.lookSensitivity),
+      worldQuat
+    );
+    this.camera.matrixWorld.compose(
+      worldPos,
+      candidateWorldQuat,
+      worldScale
+    );
+    this.camera.matrix.multiplyMatrices(parentInverse, this.camera.matrixWorld);
+    this.camera.matrix.decompose(this.camera.position, this.camera.quaternion, this.camera.scale);
+
+    // translate
+    this.direction.set(input.get(Fly.moveX), 0, input.get(Fly.moveZ));
     const boostSpeed = input.get(Fly.boost) ? this.boostSpeed : 1;
     const speed = dt * this.moveSpeed * boostSpeed;
-
-    this.camera.translateOnAxis(this.direction, speed);
+    if (this.direction.lengthSq() > EPSILON) {
+      this.camera.translateOnAxis(this.direction, speed);
+    }
   }
 }
