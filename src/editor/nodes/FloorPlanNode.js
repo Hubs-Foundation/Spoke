b75a79ed9e010b778454eeb36470acbc7892d0b2
Greg Fodor <gfodor@gmail.com>
2019-10-22 02:05:21 +0000

    Fix var name

--- src/editor/nodes/FloorPlanNode.js
+++ src/editor/nodes/FloorPlanNode.js
@@ -164,7 +164,7 @@ export default class FloorPlanNode extends EditorNodeMixin(FloorPlan) {
         agentMaxClimb: this.agentMaxClimb,
         agentMaxSlope: this.agentMaxSlope,
         regionMinSize: this.regionMinSize,
-        wasmPath: recastWasmUrl
+        wasmUrl: recastWasmUrl
       },
       signal
     );
