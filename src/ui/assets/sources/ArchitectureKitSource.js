d3f4016b3903f93337ef736bc4235e61dcaf614a
Robert Long <robert@robertlong.me>
2019-10-25 13:54:17 -0700

    Update kit and add more error messages

--- src/ui/assets/sources/ArchitectureKitSource.js
+++ src/ui/assets/sources/ArchitectureKitSource.js
@@ -3,7 +3,7 @@ import { TransformPivot } from "../../../editor/controls/SpokeControls";
 
 export default class ArchitectureKitSource extends KitSource {
   constructor() {
-    super("https://assets-prod.reticulum.io/kits/architecture/ArchKit-6969272253db32f65c0bf6327a73f471edfdb1ad.gltf");
+    super("https://assets-prod.reticulum.io/kits/architecture/ArchKit-191b8dd9b4234ded40b70a31940fb064bbdd3ecc.gltf");
     this.id = "architecture-kit";
     this.name = "Architecture Kit";
     this.experimental = true;
