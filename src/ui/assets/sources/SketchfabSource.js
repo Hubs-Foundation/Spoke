6f4660220e2f400fa3aadb0a745180078d92331c
Robert Long <robert@robertlong.me>
2019-11-12 16:24:13 -0800

    Add medieval contest collections

--- src/ui/assets/sources/SketchfabSource.js
+++ src/ui/assets/sources/SketchfabSource.js
@@ -44,14 +44,315 @@ export default class SketchfabSource extends ModelMediaSource {
             value: "medieval-fantasy-context",
             children: [
               {
-                label: "Test",
-                value: "collection-id",
+                label: "Medieval City Builder - by Eanorien",
+                value: "355623519b124985a08c4370d93a3810",
                 paramsKey: "collection",
                 initialNodeProps: {
-                  initialScale: 1
+                  initialScale: 1,
+                  castShadow: true,
+                  receiveShadow: true
                 },
                 itemProps: {
                   transformPivot: TransformPivot.Selection
+                }
+              },
+              {
+                label: "Sky Castle - by Alok",
+                value: "bdf05d92317b4bfba9ffd81fea39a368",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 1,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Selection
+                }
+              },
+              {
+                label: "Middle Ages Mine - by Vladislav Laryushin",
+                value: "d106792122154e8ba89b5f28f1889f8b",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 1,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Selection
+                }
+              },
+              {
+                label: "Medieval Labyrinth - by NomadKing",
+                value: "965f436a8aff41548cc5fa689ac114b8",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 1,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Selection
+                }
+              },
+              {
+                label: "Medieval Town Port - by lakos",
+                value: "6c227e9f36fc4558a23de3f4af9a5c80",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 1,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Selection
+                }
+              },
+              {
+                label: "Assassin Hideout - by marti3d",
+                value: "05c5c177f1c7403597c18b6c14886b63",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 1,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Bottom
+                }
+              },
+              {
+                label: "Medieval Castle with Village - by Artbake Graphics",
+                value: "39a126f30394489d9de6af7f452d553a",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 1,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Selection
+                }
+              },
+              {
+                label: "Medieval Fantasy Book - by Pixel",
+                value: "cf717f81734b4eefba9f17e9e6a1c9e4",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 1,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Bottom
+                }
+              },
+              {
+                label: "Blacksmith's Workshop - by Kyan0s",
+                value: "a013c33e56a44b479cc527e2dc992b7f",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 1,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Selection
+                }
+              },
+              {
+                label: "Castle, Siege, Army - by BlackSpire",
+                value: "53b764aab88b472ebe4ad80c4148c336",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 1,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Selection
+                }
+              },
+              {
+                label: "Treasure Island - by rogueisland",
+                value: "8f14c1d6b54d467784dfccb82aa882eb",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 1,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Bottom
+                }
+              },
+              {
+                label: "Medieval Country Village - by David",
+                value: "2c995b31f2774df1b8fac87e65b1752d",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 1,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Bottom
+                }
+              },
+              {
+                label: "Mobile Dragon Shop - by Nive",
+                value: "ef4745784f664105a76d009d56004f15",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 1,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Bottom
+                }
+              },
+              {
+                label: "The Tavern - by François Espagnet",
+                value: "9983f117e3a8471da0076e8dadce2438",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 1,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Bottom
+                }
+              },
+              {
+                label: "Dragon Attack Aftermath - by Guillermo T",
+                value: "d5445aa9dc8a457bb225c4d4ce9ce97f",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 1,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Selection
+                }
+              },
+              {
+                label: "Building Modules - by Tobal",
+                value: "ca06c20e01a1405f9bc444fe36cdc175",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 1,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Selection
+                }
+              },
+              {
+                label: "The Great Sorcerer's Room - by moyicat",
+                value: "316a36ae8dac4ab281baa79f79ef38c2",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 1,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Selection
+                }
+              },
+              {
+                label: "Plateau Sanctuary - by BenjiToddArtist",
+                value: "4409d7ed90054082bf9e626cde88a9a9",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 1,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Bottom
+                }
+              },
+              {
+                label: "Throne Room - by tuturu",
+                value: "e3ad5aebeaa84806afcc4b0efe4ae39b",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 1,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Bottom
+                }
+              },
+              {
+                label: "Dark Age Motte and Bailey - by MattMakesSwords",
+                value: "50758d11ea7146378f96440655682ce0",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 2.25,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Bottom
+                }
+              },
+              {
+                label: "Market Square - by Feyfolken",
+                value: "50b5b42e549748418f93354c785a195c",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 1,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Selection
+                }
+              },
+              {
+                label: "Cemetary Fantasy Battle - by daniel_slusarz",
+                value: "7eaba35bf3e54475b8ecefa0e7725bb8",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 0.01,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Selection
+                }
+              },
+              {
+                label: `"The Drunk Troll" Tavern - by mrrobot`,
+                value: "b0e926a29dd644e48fd4c40fd416f0b8",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 0.04,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Selection
+                }
+              },
+              {
+                label: "Siege Equipment - by Carrigan Raketic",
+                value: "cc6b26d45b2a4d45be66ccdc80d5ba86",
+                paramsKey: "collection",
+                initialNodeProps: {
+                  initialScale: 3,
+                  castShadow: true,
+                  receiveShadow: true
+                },
+                itemProps: {
+                  transformPivot: TransformPivot.Bottom
                 }
               }
             ]
