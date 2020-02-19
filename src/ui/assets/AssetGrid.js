45f2cff577e0c810a32a66d0c4cc779bb622287e
Robert Long <robert@robertlong.me>
2020-02-18 17:59:16 -0800

    Remove outdated Tooltip.hide()

--- src/ui/assets/AssetGrid.js
+++ src/ui/assets/AssetGrid.js
@@ -128,8 +128,6 @@ export default function AssetGrid({ isLoading, selectedItems, items, onSelect, o
 
   const placeObject = useCallback(
     (_, trigger) => {
-      Tooltip.hide();
-
       const item = trigger.item;
 
       const node = new item.nodeClass(editor);
