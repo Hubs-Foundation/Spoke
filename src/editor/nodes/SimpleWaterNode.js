5785efdcf39a8eb129f94c857304a43a5b88a339
shawticus <shawmakesmagic@gmail.com>
2020-06-17 09:48:14 -0700

    Fixes issue where Vanilla Spoke won't start, corejs is outdated and waternormals.jpg missing

--- src/editor/nodes/SimpleWaterNode.js
+++ src/editor/nodes/SimpleWaterNode.js
@@ -1,6 +1,6 @@
 import EditorNodeMixin from "./EditorNodeMixin";
 import SimpleWater from "../objects/SimpleWater";
-import waterNormalsUrl from "three/examples/textures/waternormals.jpg";
+import waterNormalsUrl from "../../assets/waternormals.jpg";
 import loadTexture from "../utils/loadTexture";
 import { Texture } from "three";
 
