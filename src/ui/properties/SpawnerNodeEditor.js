16d465c941182893ab29c140d74ecc60f3ae89fc
netpro2k <netpro2k@gmail.com>
2020-12-04 17:03:10 -0800

    Allow toggling floating on objects from spawners

--- src/ui/properties/SpawnerNodeEditor.js
+++ src/ui/properties/SpawnerNodeEditor.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import NodeEditor from "./NodeEditor";
 import InputGroup from "../inputs/InputGroup";
 import ModelInput from "../inputs/ModelInput";
+import BooleanInput from "../inputs/BooleanInput";
 import { Magic } from "styled-icons/fa-solid/Magic";
 import { GLTFInfo } from "../inputs/GLTFInfo";
 
@@ -20,6 +21,10 @@ export default class SpawnerNodeEditor extends Component {
     this.props.editor.setPropertiesSelected({ ...initialProps, src });
   };
 
+  onChangeModifyGravityOnRelease = modifyGravityOnRelease => {
+    this.props.editor.setPropertySelected("modifyGravityOnRelease", modifyGravityOnRelease);
+  };
+
   render() {
     const node = this.props.node;
 
@@ -27,6 +32,9 @@ export default class SpawnerNodeEditor extends Component {
       <NodeEditor {...this.props} description={SpawnerNodeEditor.description}>
         <InputGroup name="Model Url">
           <ModelInput value={node.src} onChange={this.onChangeSrc} />
+        </InputGroup>
+        <InputGroup name="Spawned object is floaty">
+          <BooleanInput value={node.modifyGravityOnRelease} onChange={this.onChangeModifyGravityOnRelease} />
         </InputGroup>
         {node.model && <GLTFInfo node={node} />}
       </NodeEditor>
