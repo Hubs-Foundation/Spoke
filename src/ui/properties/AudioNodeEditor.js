04578bbe237866436dbe50c166fdb8d1436fe268
Manuel Martin <manmartin@mozilla.com>
2021-07-29 13:21:57 +0200

    Restore lost media params

--- src/ui/properties/AudioNodeEditor.js
+++ src/ui/properties/AudioNodeEditor.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import NodeEditor from "./NodeEditor";
 import InputGroup from "../inputs/InputGroup";
 import AudioInput from "../inputs/AudioInput";
+import BooleanInput from "../inputs/BooleanInput";
 import { VolumeUp } from "styled-icons/fa-solid/VolumeUp";
 import AudioParamsProperties from "./AudioParamsProperties";
 import useSetPropertySelected from "./useSetPropertySelected";
@@ -10,11 +11,23 @@ import useSetPropertySelected from "./useSetPropertySelected";
 export default function AudioNodeEditor(props) {
   const { editor, node } = props;
   const onChangeSrc = useSetPropertySelected(editor, "src");
+  const onChangeControls = useSetPropertySelected(editor, "controls");
+  const onChangeAutoPlay = useSetPropertySelected(editor, "autoPlay");
+  const onChangeLoop = useSetPropertySelected(editor, "loop");
 
   return (
     <NodeEditor description={AudioNodeEditor.description} {...props}>
       <InputGroup name="Audio Url">
         <AudioInput value={node.src} onChange={onChangeSrc} />
+      </InputGroup>
+      <InputGroup name="Controls" info="Toggle the visibility of the media controls in Hubs.">
+        <BooleanInput value={node.controls} onChange={onChangeControls} />
+      </InputGroup>
+      <InputGroup name="Auto Play" info="If true, the media will play when first entering the scene.">
+        <BooleanInput value={node.autoPlay} onChange={onChangeAutoPlay} />
+      </InputGroup>
+      <InputGroup name="Loop" info="If true the media will loop indefinitely.">
+        <BooleanInput value={node.loop} onChange={onChangeLoop} />
       </InputGroup>
       <AudioParamsProperties {...props} />
     </NodeEditor>
