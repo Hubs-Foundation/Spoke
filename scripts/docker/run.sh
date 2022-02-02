bb27a93483024c7a316728a45bedc3a466ffa28e
Geng Tan <tanfarming@hotmail.com>
2022-02-02 12:01:38 -0500

    Update run.sh

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -1,3 +1,8 @@
+# TODO: need a better one
+healthcheck(){
+    while true; do (echo -e 'HTTP/1.1 200 OK\r\n\r\n 1') | nc -lp 1111 > /dev/null; done
+}
+
 find /www/spoke/ -type f -name *.html -exec sed -i "s~{{rawspoke-base-assets-path}}\/~${turkeyCfg_base_assets_path}~g" {} \;
 find /www/spoke/ -type f -name *.html -exec sed -i "s~{{rawspoke-base-assets-path}}~${turkeyCfg_base_assets_path}~g" {} \;
 find /www/spoke/ -type f -name *.css -exec sed -i "s~{{rawspoke-base-assets-path}}\/~${turkeyCfg_base_assets_path}~g" {} \;
@@ -10,4 +15,5 @@ for f in /www/spoke/pages/*.html; do
     [[ $var == $prefix* ]] && sed -i "s/$anchor/ <meta name=\"env:${var#$prefix}\" content=\"${!var//\//\\\/}\"\/> $anchor/" $f;
     done
 done
+healthcheck &
 nginx -g "daemon off;"
