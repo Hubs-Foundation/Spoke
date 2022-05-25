18d688a7e1f7c89b54e24f2470ece6b0a7077143
Geng Tan <geng.tan@limeade.com>
2022-05-25 12:07:13 -0400

    turkey-docker-update

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -1,13 +1,13 @@
 
-turkeyCfg_base_assets_path="https://$SUB_DOMAIN-assets.$DOMAIN/spoke/"
-turkeyCfg_cors_proxy_server="$SUB_DOMAIN-cors.$DOMAIN"
-turkeyCfg_ga_tracking_id=""
-turkeyCfg_hubs_server="$SUB_DOMAIN.$DOMAIN"
-turkeyCfg_is_moz="false"
-turkeyCfg_non_cors_proxy_domains="$SUB_DOMAIN.$DOMAIN,$SUB_DOMAIN-assets.$DOMAIN"
-turkeyCfg_reticulum_server="$SUB_DOMAIN.$DOMAIN"
-turkeyCfg_sentry_dsn=""
-turkeyCfg_thumbnail_server="nearspark.reticulum.io"
+export turkeyCfg_thumbnail_server="nearspark.reticulum.io"
+export turkeyCfg_base_assets_path="https://$SUB_DOMAIN-assets.$DOMAIN/spoke/"
+export turkeyCfg_non_cors_proxy_domains="$SUB_DOMAIN.$DOMAIN,$SUB_DOMAIN-assets.$DOMAIN"
+export turkeyCfg_cors_proxy_server="$SUB_DOMAIN-cors.$DOMAIN"
+export turkeyCfg_ga_tracking_id=""
+export turkeyCfg_hubs_server="$SUB_DOMAIN.$DOMAIN"
+export turkeyCfg_is_moz="false"
+export turkeyCfg_reticulum_server="$SUB_DOMAIN.$DOMAIN"
+export turkeyCfg_sentry_dsn=""
 
 find /www/spoke/ -type f -name *.html -exec sed -i "s~{{rawspoke-base-assets-path}}\/~${turkeyCfg_base_assets_path}~g" {} \;
 find /www/spoke/ -type f -name *.html -exec sed -i "s~{{rawspoke-base-assets-path}}~${turkeyCfg_base_assets_path}~g" {} \;
