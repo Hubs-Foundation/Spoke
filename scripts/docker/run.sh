17240e40f59a7c026a7bc70740c9fc88c7891f26
Geng Tan <geng.tan@limeade.com>
2022-05-25 10:08:06 -0400

    turkey-docker-update

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -1,3 +1,14 @@
+
+turkeyCfg_base_assets_path="https://$SUB_DOMAIN-assets.$DOMAIN/spoke/"
+turkeyCfg_cors_proxy_server="$SUB_DOMAIN-cors.$DOMAIN"
+turkeyCfg_ga_tracking_id=""
+turkeyCfg_hubs_server="$SUB_DOMAIN.$DOMAIN"
+turkeyCfg_is_moz="false"
+turkeyCfg_non_cors_proxy_domains="$SUB_DOMAIN.$DOMAIN,$SUB_DOMAIN-assets.$DOMAIN"
+turkeyCfg_reticulum_server="$SUB_DOMAIN.$DOMAIN"
+turkeyCfg_sentry_dsn=""
+turkeyCfg_thumbnail_server="nearspark.reticulum.io"
+
 find /www/spoke/ -type f -name *.html -exec sed -i "s~{{rawspoke-base-assets-path}}\/~${turkeyCfg_base_assets_path}~g" {} \;
 find /www/spoke/ -type f -name *.html -exec sed -i "s~{{rawspoke-base-assets-path}}~${turkeyCfg_base_assets_path}~g" {} \;
 find /www/spoke/ -type f -name *.css -exec sed -i "s~{{rawspoke-base-assets-path}}\/~${turkeyCfg_base_assets_path}~g" {} \;
