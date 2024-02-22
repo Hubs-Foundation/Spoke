ca231777c3734380f45f56f4085f0ff7ac79455f
$(git config --global user.name) <$(git config --global user.email)>
2024-02-22 01:57:33 +0000

    Add BACKEND_URL configuration

--- src/configs.js
+++ src/configs.js
@@ -21,6 +21,7 @@ get(configs, "SENTRY_DSN", process.env.SENTRY_DSN);
 get(configs, "GA_TRACKING_ID", process.env.GA_TRACKING_ID);
 get(configs, "BASE_ASSETS_PATH", process.env.BASE_ASSETS_PATH);
 get(configs, "IS_MOZ", process.env.IS_MOZ);
+get(configs, "BACKEND_URL", process.env.BACKEND_URL);
 
 if (configs.BASE_ASSETS_PATH) {
   // eslint-disable-next-line no-undef
--- webpack.config.js
+++ webpack.config.js
@@ -268,7 +268,8 @@ module.exports = env => {
         IS_MOZ: false,
         GITHUB_ORG: "mozilla",
         GITHUB_REPO: "spoke",
-        GITHUB_PUBLIC_TOKEN: "ghp_SAFEPB2zzes9TEpAOSx2McNjJLQ1GXLBES2FsfWU"
+        GITHUB_PUBLIC_TOKEN: "ghp_SAFEPB2zzes9TEpAOSx2McNjJLQ1GXLBES2FsfWU",
+        BACKEND_URL: ""
       })
     ]
   };
