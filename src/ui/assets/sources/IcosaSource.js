5b08d4b0a5679d053888fbdf734ce69908342a4c
Andy Baker <andy@andybak.net>
2025-09-05 12:43:27 +0100

    Update categories to match all valid choices in the Icosa API

--- src/ui/assets/sources/IcosaSource.js
+++ src/ui/assets/sources/IcosaSource.js
@@ -7,16 +7,24 @@ export default class IcosaSource extends ModelMediaSource {
     this.id = "icosa";
     this.name = "Icosa Gallery";
     this.tags = [
-      { label: "Featured", value: "" },
-      { label: "Animals", value: "animals" },
+      { label: "Animals & Pets", value: "animals" },
       { label: "Architecture", value: "architecture" },
       { label: "Art", value: "art" },
+      { label: "Culture", value: "culture" },
+      { label: "Events", value: "events" },
       { label: "Food", value: "food" },
+      { label: "History", value: "history" },
+      { label: "Home", value: "home" },
+      { label: "Miscellaneous", value: "miscellaneous" },
       { label: "Nature", value: "nature" },
       { label: "Objects", value: "objects" },
       { label: "People", value: "people" },
-      { label: "Scenes", value: "scenes" },
-      { label: "Transport", value: "transport" }
+      { label: "Places", value: "places" },
+      { label: "Science", value: "science" },
+      { label: "Sports", value: "sports" },
+      { label: "Tech", value: "tech" },
+      { label: "Transport", value: "transport" },
+      { label: "Travel", value: "travel" }
     ];
 
     this.searchLegalCopy = "Search by Icosa";
